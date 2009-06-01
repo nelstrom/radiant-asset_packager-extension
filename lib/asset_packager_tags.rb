4a6182d9f15ab7250d087316e3c5b0932b936081
Andrew Neil <andrew.jr.neil@gmail.com>
2009-06-01 13:35:34 +0100

    Added rudimentary Radius tags for stylesheet/js.

--- lib/asset_packager_tags.rb
+++ lib/asset_packager_tags.rb
@@ -1,4 +1,35 @@
+require 'synthesis/asset_package_helper'
+include Synthesis::AssetPackageHelper
+include ActionView::Helpers::TagHelper
+include ActionView::Helpers::AssetTagHelper
+
 module AssetPackagerTags
   include Radiant::Taggable
+  
+  desc %{
+    Include stylesheets from the public/stylesheets directory.
+    
+    When running in development environment, this will include 
+    each file individually. In the production environment, the 
+    individual files will be merged and minified into a single
+    file.
+  }
+  tag 'stylesheet_link_merged' do |tag|
+    # tag.locals.filter_attributes = tag.attr || {}
+    stylesheet_link_merged :base
+  end
+  
+  desc %{
+    Include javascripts from the public/javascripts directory.
+    
+    When running in development environment, this will include 
+    each file individually. In the production environment, the 
+    individual files will be merged and minified into a single
+    file.
+  }
+  tag 'javascript_include_merged' do |tag|
+    # tag.locals.filter_attributes = tag.attr || {}
+    javascript_include_merged :base
+  end
   
 end
